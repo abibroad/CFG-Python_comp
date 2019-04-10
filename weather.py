c562afb5de6f61c25afcd4a192184aeb2b912a1d
Abi Broad <abi_broad@hotmail.co.uk>
2019-04-10 19:27:09 +0100

    Update weather.py

--- weather.py
+++ weather.py
@@ -74,8 +74,8 @@ def weather_temp(input_temperature):
 
 
 #contact form
-# @flask_app.route("signup",methods=["POST"])
-# def sign_up():
-#     form_data = request.format
-#     print form_data["email"]
-#     return "You are now subscribed to our mailing list!"
+@flask_app.route("signup",methods=["POST"])
+    def sign_up():
+        form_data = request.format
+        print form_data["email"]
+        return "You are now subscribed to our mailing list!"
